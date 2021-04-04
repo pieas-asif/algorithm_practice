679a734e0677bf58ac36e17672df8b6b17779dfe
Shaman Sharif <pieas.asif4@gmail.com>
2021-04-04 19:37:27 +0600

    Solved 1

--- algo_expert_practice/main.swift
+++ algo_expert_practice/main.swift
@@ -5,7 +5,44 @@
 //  Created by Shaman Sharif on 4/4/21.
 //
 
-import Foundation
+// find two sum from array
 
-print("Hello, World!")
+// solution one
+//var hashTable = [Int: Bool]()
+//var listOfNumbers = [3,5,-4,8,11,1,-1,6]
+//var resultNumber = 10
+//
+//for num in listOfNumbers {
+//    if hashTable[resultNumber-num] != nil {
+//        print("\(resultNumber-num), \(num)")
+//        break
+//    } else {
+//        hashTable[num] = true
+//    }
+//}
+
+
+// solution two
+var listOfNumbers = [3,5,-4,8,11,1,-1,6]
+var sortedListOfNumbers = listOfNumbers.sorted()
+var resultNumber = 10
+var firstIter = 0
+var lastIter = listOfNumbers.count-1
+var currentSum: Int
+
+while true {
+    if lastIter == firstIter {
+        break
+    }
+    currentSum = listOfNumbers[firstIter] + listOfNumbers[lastIter]
+    if currentSum > resultNumber {
+        lastIter -= 1
+    } else if currentSum < resultNumber {
+        firstIter += 1
+    } else{
+        print("\(listOfNumbers[firstIter]), \(listOfNumbers[lastIter])")
+        break
+    }
+}
+
 
